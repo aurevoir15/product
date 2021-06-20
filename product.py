f55421161490580dae32b4467d898b6ae27fd9e6
aurevoir15 <aurevoir15@gmail.com>
2021-06-21 00:37:02 +0800

    add function1

--- product.py
+++ product.py
@@ -1,41 +1,50 @@
 import os  #operating system
 
-
-products = []
-if os.path.isfile('products.csv'):
-	print('yeah! 找到檔案了!')
-	with open('products.csv', 'r', encoding='utf-8') as f:
-		for line in f:
-			if '商品,價格' in line:
-				continue #跳過這一回
-			name, price = line.strip().split(',')
-			products.append([name, price])
-	print(products)
-else:
-	print('找不到檔案...')
+def read_file(filename):
+    products = []
+    if os.path.isfile(filename):
+        print('yeah! 找到檔案了!')
+        with open(filename, 'r', encoding='utf-8') as f:
+            for line in f:
+                if '商品,價格' in line:
+                    continue #跳過這一回
+                name, price = line.strip().split(',')
+                products.append([name, price])
+        print(products)
+    else:
+        print('找不到檔案...')
+       return products
 
 
 #讓使用者輸入
-while True:
-	name = input('請輸入商品名稱:')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格:')
-	#p = []
-	#p.append(name)
-	#p.append(price)
-	#p = [name, pricce]
-	#products.append(p)
-	price = int(price)
-	products.append([name, price])
-print(products[0], products[1])
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱:')
+        if name == 'q':
+            break
+        price = input('請輸入商品價格:')
+        price = int(price)
+        products.append([name, price])
+    print(products)
+    return products
 
 # 印出所有購買紀錄
-for product in products:
-	print(product[0], '的價格是', product[1])
+def print_products(products):
+    for p in products:
+        print(p[0], '的價格是', p[1])
 
 # 寫入檔案
-with open('products.csv', 'w', encoding='utf-8') as f:
-	f.write('商品,價格\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+def wirte_file(filename, products):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('商品,價格\n')
+        for p in products:
+            f.write(p[0] + ',' + str(p[1]) + '\n')
+
+
+products = read_file('products.csv')
+products = user_input(products)
+print_products(products)
+wirte_file('products.csv', products)
+
+
+
